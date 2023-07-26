ad963723b69a1c9266fec2e6cda5b8efa978dd66
Tamrazrk <135056938+Tamrazrk@users.noreply.github.com>
2023-07-26 10:25:42 +0400

    Add files via upload

---
+++ Information.py
@@ -0,0 +1,10 @@
+# Create the variables
+name = "Tamar"
+age = 25
+shoe_size = 35
+
+# Create the info sentence using the variables
+info = "Hi, my name is {} and I am {} years old. My shoe size is {}.".format(name, age, shoe_size)
+
+# Print the info message
+print(info)
